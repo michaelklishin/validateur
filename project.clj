b1ff6ed36c502cc222f53ae4b0a9e4cd3f533a5a
Michael S. Klishin <michael@novemberain.com>
2011-09-24 13:28:57 +0400

    Clojure 1.3.0 is out

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject com.novemberain/validateur "0.1.0-SNAPSHOT"
   :description "Functional validations inspired by Ruby's ActiveModel"
-  :dependencies [[org.clojure/clojure "1.3.0-RC0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
