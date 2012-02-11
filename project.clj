2f20a451861b00ba0579893299bcd0195fe4792a
Michael S. Klishin <michael@novemberain.com>
2012-02-11 09:40:45 +0400

    Now working on 1.1.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/validateur "1.0.0"
+(defproject com.novemberain/validateur "1.1.0-SNAPSHOT"
   :description "Functional validations inspired by Ruby's ActiveModel"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :test-selectors   {:focus          (fn [v] (:focus v)) })
