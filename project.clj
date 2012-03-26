5cd90d456e5cfbfa342f919cdf894a56f6e813ff
Michael S. Klishin <michael@novemberain.com>
2012-03-26 08:15:57 +0400

    Clojure 1.4.0-beta5

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Functional validations inspired by Ruby's ActiveModel"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :profiles { :1.4 { :dependencies [[org.clojure/clojure "1.4.0-beta4"]] } }
+  :profiles { :1.4 { :dependencies [[org.clojure/clojure "1.4.0-beta5"]] } }
   :aliases  { "all" ["with-profile" "dev:dev,1.4"] }
   :test-selectors {:focus (fn [v] (:focus v))})
