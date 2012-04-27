04fd9bef50fb418f5ada99643f875ed07b29c4b6
Michael S. Klishin <michael@novemberain.com>
2012-04-27 10:20:20 +0400

    Test against Clojure 1.5-master-SNAPSHOT

--- project.clj
+++ project.clj
@@ -3,10 +3,13 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure  "1.3.0"]
                  [clojurewerkz/support "0.1.0-beta3"]]
-  :profiles { :1.4 { :dependencies [[org.clojure/clojure "1.4.0"]] } }
-  :aliases  { "all" ["with-profile" "dev:dev,1.4"] }
-  :repositories {"clojure-releases" "http://build.clojure.org/releases"
-                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+  :profiles {:1.4 { :dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 { :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :aliases  {"all" ["with-profile" "dev:dev,1.4:dev,1.5"]}
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
-                             :releases {:checksum :fail :update :always}}}
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
   :test-selectors {:focus (fn [v] (:focus v))})
