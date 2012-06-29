fd024c9d99eaad2e0af73bd64ea01730062a8cbb
Michael S. Klishin <michael@novemberain.com>
2012-06-29 10:52:59 +0400

    Support 0.5.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Functional validations inspired by Ruby's ActiveModel"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure  "1.3.0"]
-                 [clojurewerkz/support "0.4.0"]]
+                 [clojurewerkz/support "0.5.0"]]
   :profiles {:1.4 { :dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 { :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
   :aliases  {"all" ["with-profile" "dev:dev,1.4:dev,1.5"]}
