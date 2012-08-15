909dbc99e3dc578a4e83bc54fcef61f9ff7a3cee
Michael S. Klishin <michael@novemberain.com>
2012-08-15 10:35:46 +0400

    1.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/validateur "1.2.0"
+(defproject com.novemberain/validateur "1.3.0-SNAPSHOT"
   :description "Functional validations inspired by Ruby's ActiveModel"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure  "1.3.0"]
