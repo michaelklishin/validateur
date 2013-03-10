0b59876b242a10bb15cbdd76f9699bbe2f9a5f50
Michael S. Klishin <michael@novemberain.com>
2013-03-10 15:41:55 +0400

    Upgrade to Clojure 1.5, bump dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,16 @@
 (defproject com.novemberain/validateur "1.3.0-SNAPSHOT"
   :description "Functional validations inspired by Ruby's ActiveModel"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure  "1.4.0"]
-                 [clojurewerkz/support "0.7.0"]]
+  :dependencies [[org.clojure/clojure  "1.5.0"]
+                 [clojurewerkz/support "0.14.0"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
-             :dev {:plugins [[codox "0.6.1"]]
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :dev {:plugins [[codox "0.6.4"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases  {"all" ["with-profile" "dev:dev,1.3:dev,1.5"]}
+  :aliases  {"all" ["with-profile" "dev:dev,1.3:dev,1.4:dev,1.6:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
