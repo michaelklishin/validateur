7f7937abe6beb1a3ed8b6e20f9811b2eb00ebd76
Michael Klishin <michael@clojurewerkz.org>
2014-12-14 17:34:15 +0300

    Drop Clojure 1.4 profile

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
   :min-lein-version "2.5.0"
   :dependencies [[org.clojure/clojure  "1.6.0"]]
   :jar-exclusions [#"\.cljx"]
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:dependencies [[org.clojure/clojurescript "0.0-2138"]]
@@ -34,7 +33,7 @@
                                                     :optimizations :advanced}}]}
                    :codox {:sources ["src/cljx" "target/classes"]
                            :output-dir "doc/api"}}}
-  :aliases  {"all" ["with-profile" "+dev:+1.4:+1.5:+1.7:+master"]}
+  :aliases  {"all" ["with-profile" "+dev:+1.5:+1.7:+master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
